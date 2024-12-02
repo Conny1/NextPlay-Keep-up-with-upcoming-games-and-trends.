54f08f51850b2264b30c0b788b16deebf6770318
Conrad Joel Mbuya <joelconrad277@gmail.com>
2024-12-02 22:40:22 +0300

    profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "expo-router";
 
 const Profile = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({ headerShown: true, headerTitle: "Profile" });
+  }, []);
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.mainContainer}>
+      <View>
+        <Image
+          source={{
+            uri: "https://img.freepik.com/free-photo/close-up-studio-photo-elite-unit-special-forces-soldier-camouflage-uniform-holding-assault-rifle-with-laser-sight-aims-target_613910-20395.jpg?t=st=1732735261~exp=1732738861~hmac=6b6f81a6f83bb5e03d85bd31599312957fd3fb2b56188433fa40d20bc89d08c0&w=1380",
+          }}
+          height={100}
+          width={100}
+          style={{ resizeMode: "cover", borderRadius: 90 }}
+        />
+        <Text>Alex</Text>
+        <Text>alex@gmail.com</Text>
+        <Text style={{ fontSize: 20, marginBottom: 20, fontWeight: "bold" }}>
+          Wishlist
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    margin: 15,
+  },
+});
--- app/game/[id].tsx
+++ app/game/[id].tsx
@@ -1,7 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useEffect } from "react";
 import { useNavigation } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { Rating, AirbnbRating } from "react-native-ratings";
 
 const Gamescreen = () => {
   const navigation = useNavigation();
@@ -10,29 +18,79 @@ const Gamescreen = () => {
   }, []);
 
   return (
-    <View style={styles.mainContainer}>
-      <View style={styles.starContainer}>
-        <AntDesign
-          style={{ backgroundColor: "#dae0e4", padding: 3, borderRadius: 7 }}
-          name="staro"
-          size={24}
-          color="black"
-        />
-        <Text>Action</Text>
-        <Text>Adventure</Text>
-      </View>
+    <ScrollView>
+      <View style={styles.mainContainer}>
+        <View style={styles.starContainer}>
+          <AntDesign
+            style={{ backgroundColor: "#dae0e4", padding: 3, borderRadius: 7 }}
+            name="staro"
+            size={24}
+            color="black"
+          />
+          <Text>Action,</Text>
+          <Text>Adventure</Text>
+        </View>
 
-      <Text style={{ fontSize: 20, marginBottom: 10, fontWeight: "bold" }}>
-        About the game
-      </Text>
-      <Text style={{ lineHeight: 18 }}>
-        Set in the dystopian Night City, Cyberpunk 2077 offers a richly
-        detailed, futuristic world filled with advanced technology and societal
-        decay. Players take on the role of V, a customizable mercenary,
-        navigating a story that involves themes of transhumanism, corporate
-        power, and rebellion.
-      </Text>
-    </View>
+        <View>
+          <Text style={{ fontSize: 20, marginBottom: 20, fontWeight: "bold" }}>
+            About the game
+          </Text>
+          <Text style={{ lineHeight: 18, marginBottom: 20 }}>
+            Set in the dystopian Night City, Cyberpunk 2077 offers a richly
+            detailed, futuristic world filled with advanced technology and
+            societal decay. Players take on the role of V, a customizable
+            mercenary, navigating a story that involves themes of transhumanism,
+            corporate power, and rebellion.
+          </Text>
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 15 }}>Release Date: Dec, 9, 2020</Text>
+          <Text style={{ lineHeight: 18, color: "gray", marginBottom: 10 }}>
+            Platforms: Playstation 5, Xbox 1,Miscrosoft windows, Google stadia,
+            Playstation 4
+          </Text>
+        </View>
+        <Image
+          source={{
+            uri: "https://img.freepik.com/free-photo/close-up-studio-photo-elite-unit-special-forces-soldier-camouflage-uniform-holding-assault-rifle-with-laser-sight-aims-target_613910-20395.jpg?t=st=1732735261~exp=1732738861~hmac=6b6f81a6f83bb5e03d85bd31599312957fd3fb2b56188433fa40d20bc89d08c0&w=1380",
+          }}
+          height={200}
+          // width={100}
+          style={{ width: "100%", resizeMode: "cover" }}
+        />
+
+        <View>
+          <Rating
+            ratingCount={10}
+            imageSize={30}
+            showRating
+            type="custom"
+            ratingColor="black"
+            ratingBackgroundColor="#e9e1e1"
+            style={{ backgroundColor: "inherit" }}
+            tintColor="#ffff"
+          />
+        </View>
+        <TouchableOpacity
+          style={{
+            marginTop: 20,
+            padding: 7,
+            display: "flex",
+            width: "100%",
+            flexDirection: "row",
+            backgroundColor: "#1e90ff",
+            borderRadius: 5,
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
+        >
+          <AntDesign name="hearto" size={24} color="white" />
+          <Text style={{ color: "#fff" }}>Add to wishlist</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
